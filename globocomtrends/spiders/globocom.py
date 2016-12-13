16bcb950dbf7d0c90492f94cf4c349e39a142e84
Leonardo Cesar Borges <leonardo.borges@neoway.com.br>
2016-12-13 21:20:40 -0200

    first scrapy version

---
+++ globocomtrends/spiders/globocom.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+
+class GlobocomSpider(CrawlSpider):
+    name = 'globocom'
+    allowed_domains = ['globo.com']
+    start_urls = ['http://globo.com/']
+
+    rules = (
+        Rule(LinkExtractor(allow=r'.html'), callback='parse_item', follow=True),
+    )
+
+    def parse_item(self, response):
+        i = {}
+        i['title'] = response.xpath('//title').extract()
+        return i
